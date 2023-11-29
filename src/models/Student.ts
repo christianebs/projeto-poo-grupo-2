6f8690b4d49dc55d2c7a08044e17b423fad457c6
Marília Pinheiro Alves <mariliapinheiroalves@gmail.com>
2023-11-28 21:50:45 -0300

    Update src/models Student.ts

--- src/models/Student.ts
+++ src/models/Student.ts
@@ -1,58 +1,54 @@
-import { StudentProps } from "../types/StudentProps";
-import { database } from "..";
+import { StudentProps } from "../types/StudentProps"
+import { database } from ".."
 
 export class Student implements StudentProps {
-    private _id: number
-    private _name: string
-    private _age: number
-    private _course: Array<number>
+  private _id: number
+  private _name: string
+  private _age: number
+  private _course: number
 
-    constructor(
-        name: string,
-        age: number,
-        id?: number,
-        course?: Array<number>
-    ) {
-        this._id = id || database.students.length + 1
-        this._name = name
-        this._age = age
-        this._course = course || []
-    }
+  constructor(name: string, age: number, course: number, id?: number) {
+    this._id = id || database.getNextId("student")
+    this._name = name
+    this._age = age
+    this._course = course
+  }
 
-    get id(): number {
-        return this._id
-    }
+  get id(): number {
+    return this._id
+  }
 
-    get name(): string {
-        return this._name
-    }
+  get name(): string {
+    return this._name
+  }
 
-    set name(value: string) {
-        this._name = value
-    }
+  set name(value: string) {
+    this._name = value
+  }
 
-    get age(): number {
-        return this._age
-    }
+  get age(): number {
+    return this._age
+  }
 
-    set age(value: number) {
-        this._age = value
-    }
+  set age(value: number) {
+    this._age = value
+  }
 
-    get course(): Array<number> {
-        return this._course
-    }
+  get course(): number {
+    return this._course
+  }
 
-    toString(): string {
-        const student = `\n------------------------------\n
+  toString(): string {
+    const course = database.getById("course", this.course)
+
+    const student = `\n------------------------------\n
     ID: ${this.id}\n
     Nome: ${this.name}\n
     Idade: ${this.age}\n
-    Curso: ${this.course}
+    Curso: ${course?.name}
     \n------------------------------\n
     `
-    //falta puxar disciplinas
 
-        return student
-    }
+    return student
+  }
 }
