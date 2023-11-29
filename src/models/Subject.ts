87b8835e42f3fbadd1e981371b52a401323f5143
Marília Pinheiro Alves <mariliapinheiroalves@gmail.com>
2023-11-28 21:51:42 -0300

    Update src/models Subject.ts

--- src/models/Subject.ts
+++ src/models/Subject.ts
@@ -1,61 +1,62 @@
 import { database } from ".."
-import { SubjectProps } from "../types/SubjectProps";
+import { SubjectProps } from "../types/SubjectProps"
 
 export class Subject implements SubjectProps {
-  private _id: number;
-  private _name: string;
-  private _workload: string;
-  private _description: string;
-  private _idCourse: number;
+  private _id: number
+  private _name: string
+  private _workload: string
+  private _description: string
+  private _idCourse: number
 
   constructor(
     name: string,
     workload: string,
     description: string,
     idCourse: number,
-    id?: number,
+    id?: number
   ) {
-    this._id = id || database.subjects.length + 1;
-    this._name = name;
-    this._description = description;
-    this._workload = workload;
-    this._idCourse = idCourse;
+    this._id = id || database.getNextId("subject")
+    this._name = name
+    this._description = description
+    this._workload = workload
+    this._idCourse = idCourse
   }
 
   get id(): number {
-    return this._id;
+    return this._id
   }
 
   get name(): string {
-    return this._name;
+    return this._name
   }
 
   set name(value: string) {
-    this._name = value;
+    this._name = value
   }
 
   get workload(): string {
-    return this._workload;
+    return this._workload
   }
 
   set workload(value: string) {
-    this._workload = value;
+    this._workload = value
   }
 
   get description(): string {
-    return this._description;
+    return this._description
   }
 
-  set description(value: string){
-    this._description = value;
+  set description(value: string) {
+    this._description = value
   }
 
   get idCourse(): number {
-    return this._idCourse;
+    return this._idCourse
   }
 
   toString(): string {
-    const course = database.courses.find((course) => course.id === this.idCourse)
+    const course = database.getById("course", this._idCourse)
+
     const subject = `\n------------------------------\n
     ID: ${this.id}\n
     Nome: ${this.name}\n
@@ -63,8 +64,8 @@ export class Subject implements SubjectProps {
     Descrição: ${this.description}\n
     Curso: ${course?.name}
     \n------------------------------\n
-    `;
+    `
 
-    return subject;
+    return subject
   }
 }
